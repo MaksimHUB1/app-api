31748d256b3265080b3cd6b1cd339853423aaa3c
kingl <kinglobster1234@gmail.com>
2022-08-15 14:49:09 +0300

    error check

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -9,7 +9,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 from core.models import Recipe
-from recipe.serializers import (
+from .serializers import (
     RecipeSerializer,
     RecipeDetailSerializer,
 )
